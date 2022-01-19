d40914f78dd0f542275a9b55cc4d617683df2434
jmt2110 <jthomas863@students.mchenry.edu>
2022-01-19 12:13:56 -0600

    Intial changes

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Jacob Thomas");
+        System.out.println("I am 25 years old as of Jan 19th 2022");
+        System.out.println("I am currently in the US Army Reserves");
+        System.out.println("I live in Mchenry, Illinois with my Fiance");
+    }
+}
